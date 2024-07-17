7f43d109b42d1916a8f0a235687b3b665df3d754
erme07 <erik.medina07@gmail.com>
2024-07-17 09:17:21 -0300

    add heders

---
+++ include/colisiones.h
@@ -0,0 +1,8 @@
+#ifndef COLISIONES_H
+#define COLISIONES_H
+
+#include <windows.h>
+
+//BOOL colisionBola(Serpiente*, Bloque*);
+
+#endif //COLISIONES_H
---
+++ include/consola.h
@@ -0,0 +1,10 @@
+#ifndef CONSOLA_H
+#define CONSOLA_H
+
+#include <windows.h>
+
+void moverCursor(int, int);
+void hideCursor(void);
+void cambiarColorFuente(WORD);
+
+#endif //CONSOLA_H
