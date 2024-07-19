8634d83b0f04ede7fbecac1864b352153cfc1686
erme07 <erik.medina07@gmail.com>
2024-07-19 15:48:49 -0300

    update

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 #include "include/estructuras.h"
 #include "include/menu.h"
 
-#define FRAMERATE 16 //60fps aprox
+#define FRAMERATE 20 //60fps aprox
 //#define VELOCIDAD 80
 
 enum COLISIONES{
@@ -24,7 +24,7 @@ enum COLISIONES{
 
 void imprimirBloque(Bloque*);
 void limpiarBloque(int, int);
-Bloque *generarBola(Serpiente*,int);
+Bloque *generarBola(Serpiente*,int,int);
 
 Serpiente *crearSerpiente(void);
 Bloque *crearBloque(Coordenadas,char,int);
@@ -40,35 +40,81 @@ void comerBola(Serpiente*,Bloque*,Coordenadas);
 Coordenadas posicionarCabeza(int);
 
 int detectarColisiones(Serpiente*,Bloque*,int);
+void estadisticas(Serpiente*,int,int);
+int establecerVelocidad(int,int);
+int definirNivel(Serpiente*);
+
+LARGE_INTEGER inicio, fin, frecuencia;
+
+// Función para iniciar el cronómetro
+void iniciarCronometro() {
+    QueryPerformanceFrequency(&frecuencia);
+    QueryPerformanceCounter(&inicio);
+		cambiarColorFuente(0xE);
+		moverCursor(9, 10);
+		printf("00:00");
+}
+
+// Función para obtener el tiempo transcurrido en segundos
+double obtenerTiempo() {
+    QueryPerformanceCounter(&fin);
+    return (double)(fin.QuadPart - inicio.QuadPart) / frecuencia.QuadPart;
+}
+
+// Función para imprimir el tiempo transcurrido
+void imprimirCronometro(int *segundos,int*minutos) {
+	double tiempo = obtenerTiempo();
+  *minutos = (int)tiempo / 60;
+	if(*minutos>60)
+		*minutos = 0;
+	*segundos = (int)tiempo % 60;
+	cambiarColorFuente(0xE);
+	moverCursor(9,10);
+  printf("%02d:%02d",*minutos,*segundos);
+}
+
 
 int main(){
 	int direccion = IZQUIERDA;
-	int VELOCIDAD = 20;
-	int ESCENARIO = ESCENARIO2;
+	int VELOCIDAD = 100;
+	int ESCENARIO = ESCENARIO1;
 	int CICLOS = 0;
+	int segundos, minutos;
+	int puntaje=0, nivel=1;
 	BOOL game_over = FALSE;
 	BOOL start = FALSE;
+	BOOL btemporal = FALSE;
+	BOOL bmovil = FALSE;
 	int portalH=0,portalV=0;
 	Coordenadas cola;
 
 	srand(time(NULL));
 	hideCursor();
 	dibujarEscenario(ESCENARIO);
+	disableQuickEditMode();
 	start = iniciarJuego(&ESCENARIO);
 	
 	Serpiente *serpiente = crearSerpiente();
-	Bloque *bloque = generarBola(serpiente,ESCENARIO);
+	Bloque *bloque = generarBola(serpiente,ESCENARIO,0x0F);
+	Bloque *bolatemporal,*bolamovil;
+	double intervaloActualizacion;
+	double ultimoTiempoActualizado;
+
 	if(start){
 		inicializarSerpiente(serpiente);
 		imprimirSerpiente(serpiente);
+		iniciarCronometro();
+		intervaloActualizacion = 0.5; // Actualiza el tiempo en pantalla cada 0.5 segundos.
+  	ultimoTiempoActualizado = 0;
 	}
+
 	while(!game_over && start){
 		cola.x = serpiente->cola->posicion.x;
 		cola.y = serpiente->cola->posicion.y;
 
 		if(GetAsyncKeyState(VK_UP) & 0x8000){
 			if(direccion == ARRIBA)
-				VELOCIDAD = 15;
+				VELOCIDAD = 20;
 			else if(direccion != ABAJO){
 				direccion = ARRIBA;
 				actualizarCoordenadas(serpiente, direccion);
@@ -78,7 +124,7 @@ int main(){
 					case COLISION_BOLA:
 						comerBola(serpiente, bloque, cola);
 						Sleep(VELOCIDAD);
-						bloque = generarBola(serpiente,ESCENARIO);
+						bloque = generarBola(serpiente,ESCENARIO,0x0F);
 						break;
 					case COLISION_ESCENARIO:
 						game_over = TRUE;
@@ -116,7 +162,7 @@ int main(){
 		}
 		else if(GetAsyncKeyState(VK_DOWN) & 0x8000) {
 			if(direccion == ABAJO)
-				VELOCIDAD = 15;
+				VELOCIDAD = 20;
 			else if(direccion != ARRIBA){
 				direccion = ABAJO;
 				actualizarCoordenadas(serpiente, direccion);
@@ -126,7 +172,7 @@ int main(){
 					case COLISION_BOLA:
 						comerBola(serpiente, bloque, cola);
 						Sleep(VELOCIDAD);
-						bloque = generarBola(serpiente,ESCENARIO);
+						bloque = generarBola(serpiente,ESCENARIO,0x0F);
 						break;
 					case COLISION_ESCENARIO:
 						game_over = TRUE;
@@ -174,7 +220,7 @@ int main(){
 					case COLISION_BOLA:
 						comerBola(serpiente, bloque, cola);
 						Sleep(VELOCIDAD);
-						bloque = generarBola(serpiente,ESCENARIO);
+						bloque = generarBola(serpiente,ESCENARIO,0x0F);
 						break;
 					case COLISION_ESCENARIO:
 						game_over = TRUE;
@@ -222,7 +268,7 @@ int main(){
 					case COLISION_BOLA:
 						comerBola(serpiente, bloque, cola);
 						Sleep(VELOCIDAD);
-						bloque = generarBola(serpiente,ESCENARIO);
+						bloque = generarBola(serpiente,ESCENARIO,0x0F);
 						break;
 					case COLISION_ESCENARIO:
 						game_over = TRUE;
@@ -265,7 +311,7 @@ int main(){
 				case COLISION_BOLA:
 					comerBola(serpiente, bloque, cola);
 					Sleep(VELOCIDAD);
-					bloque = generarBola(serpiente,ESCENARIO);
+					bloque = generarBola(serpiente,ESCENARIO,0x0F);
 					break;
 				case COLISION_ESCENARIO:
 					game_over = TRUE;
@@ -300,22 +346,50 @@ int main(){
 			}
 			CICLOS = 0;
 		}
-		else if(GetAsyncKeyState('0') & 0x8000) {
+		else if(GetAsyncKeyState(VK_SPACE) & 0x8000) {
 			game_over = TRUE;
 		}
-		// moverCursor(COLS+2,2);
-		// printf("%d", portalH);
-		// moverCursor(COLS+2,3);
-		// printf("%d", portalV);
 
 		imprimirBloque(bloque);
 		Sleep(FRAMERATE);  // Para evitar usar el 100% de la CPU
 		CICLOS += FRAMERATE;
-		if(direccion == ARRIBA || direccion == ABAJO)
-			VELOCIDAD = 100;
-		else VELOCIDAD = 80;
-	}
 
+		nivel = definirNivel(serpiente);
+		VELOCIDAD = establecerVelocidad(direccion, nivel);
+
+		//puntaje = (serpiente->largo - 5) * 10 *nivel;
+
+		estadisticas(serpiente,puntaje,nivel);
+		double tiempoActual = obtenerTiempo();
+		if (tiempoActual - ultimoTiempoActualizado >= intervaloActualizacion) {
+				imprimirCronometro(&segundos,&minutos);
+				ultimoTiempoActualizado = tiempoActual;
+		}
+		
+		if(!btemporal && (segundos == 10 ||segundos==25)){
+			bolatemporal = generarBola(serpiente,ESCENARIO,0x0E);
+			btemporal = TRUE;
+		}
+		if(btemporal && (segundos == 14 ||segundos==29)){
+			limpiarBloque(bolatemporal->posicion.x,bolatemporal->posicion.y);
+			btemporal = FALSE;
+			free(bolatemporal);
+		}
+
+
+		if(!bmovil && (segundos == 40)){
+			bolamovil = generarBola(serpiente, ESCENARIO, 0x6);
+			bmovil = TRUE;
+		}
+		if(bmovil && (segundos==50)){
+			limpiarBloque(bolamovil->posicion.x,bolamovil->posicion.y);
+			bmovil = FALSE;
+			free(bolamovil);
+		}
+
+		
+	}
+	
 	liberarMemoria(serpiente);
 	free(bloque);
 	moverCursor(2,ROWS+2);
@@ -383,7 +457,7 @@ Coordenadas coordenadasAleatorias(){
 	return c;
 }
 
-Bloque *generarBola(Serpiente*s,int ESCEN){
+Bloque *generarBola(Serpiente*s,int ESCEN,int color){
 	Bloque *aux = s->cola;
 	Coordenadas c = coordenadasAleatorias();
 	BOOL coordenadasOcupadas;
@@ -403,10 +477,11 @@ Bloque *generarBola(Serpiente*s,int ESCEN){
 			aux = aux->anterior;
 		}
 	} while(coordenadasOcupadas); // Repetir si las coordenadas están ocupadas
-	Bloque *bola = crearBloque(c,254,0x04); //254 craracter ascii
+	Bloque *bola = crearBloque(c,254,color); //254 craracter ascii
 	imprimirBloque(bola);
 	return bola;
 }
+
 
 void imprimirBloque(Bloque *b){
 	cambiarColorFuente(b->color);
@@ -505,3 +580,75 @@ void imprimirGameOver(){
 	printf("█▄▄█ █  █ █   █ █▄▄▄  ▀██▀ ▀██▀ █▄▄▄ █  █");
 }
 
+void estadisticas(Serpiente*s,int puntaje,int nivel){
+  cambiarColorFuente(0x0F);
+  moverCursor(5, 5);
+  printf("Puntaje: ");
+	cambiarColorFuente(0xE);
+	printf("%d", puntaje);
+  moverCursor(7, 6);
+	cambiarColorFuente(0x0F);
+  printf("Largo: ");
+	cambiarColorFuente(0xE);
+	printf("%d", s->largo);
+	moverCursor(7, 7);
+	cambiarColorFuente(0xF);
+  printf("Nivel: ");
+	cambiarColorFuente(0xE);
+	printf("%d", nivel);
+	cambiarColorFuente(0xF);
+	moverCursor(5, 15);
+  printf("%c: 10pts x Nvl", 254);
+  moverCursor(5, 16);
+  cambiarColorFuente(0xE);
+  printf("%c: 25pts x Nvl", 254);
+  moverCursor(5, 17);
+  cambiarColorFuente(0x6);
+  printf("%c: 50pts x Nvl", 254);
+  cambiarColorFuente(0xF);
+	moverCursor(4, 12);
+	printf("pausar/reanudar");
+	moverCursor(7, 13);
+	printf("(ESPACIO)");
+}
+
+int establecerVelocidad(int direccion,int nivel){
+	if(nivel==1){
+		if(direccion == ARRIBA || direccion == ABAJO)
+			return 120;
+		else return 100;
+	}
+	if(nivel==2){
+		if(direccion == ARRIBA || direccion == ABAJO)
+			return 100;
+		else return 80;
+	}
+	if(nivel==3){
+		if(direccion == ARRIBA || direccion == ABAJO)
+			return 80;
+		else return 60;
+	}
+	if(nivel==4){
+		if(direccion == ARRIBA || direccion == ABAJO)
+			return 70;
+		else return 40;
+	}
+	if(nivel==5){
+		if(direccion == ARRIBA || direccion == ABAJO)
+			return 40;
+		else return 20;
+	}
+	return 100;
+}
+
+int definirNivel(Serpiente*s){
+	if(s->largo>=100)
+		return 5;
+	if(s->largo>=90)
+		return 4;
+	if(s->largo>=60)
+		return 3;
+	if(s->largo>=30)
+		return 2;
+	return 1;
+}
