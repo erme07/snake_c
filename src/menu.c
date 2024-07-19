edd2627d9b13e88469c612a18b0e81ad8fbcc8e4
erme07 <erik.medina07@gmail.com>
2024-07-19 15:49:05 -0300

    update

--- src/menu.c
+++ src/menu.c
@@ -6,52 +6,67 @@
 
 int menu(){
   int opcion = 1,tecla;
-  char opc1=62, opc2=' ', opc3=' ';
+  char opc1=62, opc2=' ', opc3=' ', opc4=' ';
   do{ 
-    moverCursor(5, 7);
+    moverCursor(5, 8);
     if(opcion==1)
       cambiarColorFuente(0x0C);
     else
       cambiarColorFuente(0x0A);
     printf("%c Cl%csico",opc1,160);
-    moverCursor(5, 8);
+    moverCursor(5, 9);
     if(opcion==2)
       cambiarColorFuente(0x0C);
     else
       cambiarColorFuente(0x0A);
     printf("%c Portales",opc2);
-    moverCursor(5, 9);
+    moverCursor(5, 10);
     if(opcion==3)
       cambiarColorFuente(0x0C);
     else
       cambiarColorFuente(0x0A);
     printf("%c Obst%cculos",opc3,160);
-    moverCursor(5, 13);
+    if(opcion==4)
+      cambiarColorFuente(0x0C);
+    else
+      cambiarColorFuente(0x0A);
+    moverCursor(5, 12);
+    printf("%c Salir",opc4);
+    moverCursor(4, 15);
     cambiarColorFuente(0x0A);
-    printf("Presione ENTER");
-    moverCursor(6, 14);
-    printf("para iniciar");
+    printf("Elija una opcion,");
+    moverCursor(5, 16);
+    printf("presione ENTER");
     cambiarColorFuente(0x0F);
     tecla = getch();
     if(tecla==72 && opcion>1){
       opcion--;
-    }else if(tecla==80 && opcion<3){
+    }else if(tecla==80 && opcion<4){
       opcion++;
     }
     if(opcion==1){
       opc1=62;
       opc2=' ';
       opc3=' ';
+      opc4=' ';
     }else if(opcion==2){
+      if(opc3==62)
+        limpiarEscenario();
       opc1=' ';
       opc2=62;
       opc3=' ';
+      opc4=' ';
     }else if(opcion==3){
       opc1=' ';
       opc2=' ';
       opc3=62;
+      opc4=' ';
+    }else if(opcion==4){
+      opc1=' ';
+      opc2=' ';
+      opc3=' ';
+      opc4=62;
     }
-    limpiarEscenario();
     dibujarEscenario(opcion);
   } while (tecla != ENTER);
   return opcion;
@@ -89,7 +104,6 @@ void pintarMarco(){
 	}
 }
 
-
 void imprimirLogo(){
   cambiarColorFuente(0x0F);
   moverCursor(18+OFFSETX, 8+OFFSETY);
@@ -111,14 +125,15 @@ void borrarLogo(){
 }
 
 void borrarMenu(){
-  moverCursor(5, 7);
-  printf("         ");
   moverCursor(5, 8);
-  printf("          ");
+  printf("         ");
   moverCursor(5, 9);
+  printf("          ");
+  moverCursor(5, 10);
   printf("            ");
-  moverCursor(5, 13);
+  moverCursor(4, 15);
+  printf("                 ");
+  moverCursor(5, 16);
   printf("              ");
-  moverCursor(6, 14);
-  printf("            ");
 }
+
